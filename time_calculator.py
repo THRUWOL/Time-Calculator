2eaacb4a423fcb321f4df6ca32d8c87c55a24d0f
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-01-29 18:49:52 +0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,32 +1,59 @@
-def add_time(start, duration):
+def add_time(start, duration, day = ''):
     hours = int(start.split()[0].split(":")[0])
     minutes = int(start.split()[0].split(":")[1])
     timeInterval = start.split()[1]
     addHours = int(duration.split(":")[0])
     addMinutes = int(duration.split(":")[1])
+    time = ''
+    dayLeft = ''
+    dayChange = False
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    weekday = {"monday": 0,
+               "tuesday": 1,
+               "wednesday": 2,
+               "thursday": 3,
+               "friday": 4,
+               "saturday": 5,
+               "sunday": 6
+               }
 
-    days = 0
-
+    if timeInterval == 'PM':
+        dayChange = True
     newHours = hours + addHours
     newMinutes = minutes + addMinutes
+
     if newMinutes >= 60:
         newHours += newMinutes // 60
-        newMinutes = newMinutes - (newMinutes // 60)*60
+        newMinutes = newMinutes - (newMinutes // 60) * 60
 
-    days = round(newHours / 24)
-    while newHours >= 13:
+    daysCounter = round(newHours / 24)
+
+    while newHours >= 12:
         newHours = newHours - 12
         if timeInterval == 'AM':
             timeInterval = 'PM'
         else:
             timeInterval = 'AM'
 
-    print(newHours, timeInterval, newMinutes, days)
+    if len(str(newMinutes)) == 1:
+        newMinutes = '0' + str(newMinutes)
 
+    time += str(newHours) + ":" + str(newMinutes) + " " + timeInterval
 
+    if daysCounter == 1 and dayChange:
+        dayLeft = '(next day)'
+    elif daysCounter > 1:
+        dayLeft = f'({daysCounter} days later)'
 
-
-# print("11:06 PM", "2:02")
-# 1:08 AM (next day)
-print(add_time("6:30 PM", "205:12"))
-print(add_time("11:43 PM", "24:20"))
+    if len(day) > 0 and day.lower() in weekday:
+        if daysCounter > 0 and dayChange:
+            index = (weekday[day.lower()] + daysCounter) % 7
+            day = week[index]
+            return(f'{time}, {day} {dayLeft}')
+        else:
+            return(f'{time}, {day} {dayLeft}')
+    else:
+        if len(dayLeft) > 0:
+            return(f'{time} {dayLeft}')
+        else:
+            return(time)
